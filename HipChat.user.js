b2984672130b33540c9b2b6967de65096f7814ea
Árpád Szász <arpad.szasz@plenum.ro>
2013-08-06 19:14:02 +0200

    Set counter on launcher

--- HipChat.user.js
+++ HipChat.user.js
@@ -5,24 +5,50 @@
 // @author        Arpad Szasz
 // @require       utils.js
 // ==/UserScript==
- 
+
 window.Unity = external.getUnityObject(1.0);
- 
+
 function isCorrectPage() {
    var i, ids = ['roster', 'tab_lobby'];;
- 
+
    for (i = 0; i < ids.length; i++) {
        if (!document.getElementById(ids[i])) {
            return false;
        }
    }
- 
+
    return true;
 }
 
-function setupHipChat() {
+function getAlertCount () {
+    var alert_count = 0;
+    var rooms = document.getElementById('tabs').getElementsByTagName('li');
+    for (var i = 0; i < rooms.length; i++){
+        if(rooms[i].className == 'alert') {
+            alert_count++;
+        }
+    }
+
+    if (alert_count > 0) {
+        Unity.Launcher.setCount(Number(alert_count));
+    } else {
+        Unity.Launcher.clearCount();
+    }
+
+    return true;
 }
- 
+
+function setupHipChat() {
+    if (!isCorrectPage()) {
+        return;
+    }
+
+    setInterval(wrapCallback(getAlertCount), 2000);
+    getAlertCount();
+
+    return true;
+}
+
 if (isCorrectPage()) {
     Unity.init({
         name: 'HipChat',
